31559c5dd5214b0013035a875816d6c999e246b7
Christian Hartzell <christian.hartzell@fwcsstudents.org>
2025-05-07 08:20:53 -0400

    Protect user data

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,21 +13,28 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 
+@login_required
 def topics(request):
     """Show all the topics for the Learning Logs website."""
-    topic_list = Topic.objects.order_by("date_added")
-    context = {"topics": topic_list}
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
+    context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
 
 
+@login_required
 def topic(request, topic_id):
     """Show a topic and all of its entries."""
-    topic_chosen = Topic.objects.get(id=topic_id)
-    entries = topic_chosen.entry_set.order_by("-date_added")
-    context = {"topic": topic_chosen, "entries": entries}
+    topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
+
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
 
 
+@login_required
 def new_topic(request):
     """Add a new topic to the list."""
     if request.method != "POST":
@@ -34,7 +44,9 @@ def new_topic(request):
         # Process the data submitted.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect("learning_logs:topics")
 
     # Display the form if it is blank or invalid.
@@ -42,9 +54,10 @@ def new_topic(request):
     return render(request, "learning_logs/new_topic.html", context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry to a topic."""
-    chosen_topic = Topic.objects.get(id=topic_id)
+    topic = Topic.objects.get(id=topic_id)
 
     if request.method != "POST":
         # If there isn't data, create a new topic.
@@ -54,19 +67,23 @@ def new_entry(request, topic_id):
         form = EntryForm(data=request.POST)
         if form.is_valid():
             entry = form.save(commit=False)
-            entry.topic = chosen_topic
+            entry.topic = topic
             entry.save()
             return redirect("learning_logs:topic", topic_id=topic_id)
 
     # Display the form if it is blank or invalid.
-    context = {"topic": chosen_topic, "form": form}
+    context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
-    chosen_topic = entry.topic
+    topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != "POST":
         # Fill the form with the current entry by default.
@@ -76,7 +93,7 @@ def edit_entry(request, entry_id):
         form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect("learning_logs:topic", topic_id=chosen_topic.id)
+            return redirect("learning_logs:topic", topic_id=topic.id)
 
-    context = {"entry": entry, "topic": chosen_topic, "form": form}
+    context = {"entry": entry, "topic": topic, "form": form}
     return render(request, "learning_logs/edit_entry.html", context)
